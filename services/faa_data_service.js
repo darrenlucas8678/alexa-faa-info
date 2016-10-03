b02591ec7665c7ec9f834744a5c5c0f45819d3d2
Lucas <Darren.Lucas@us.sogeti.com>
2016-10-03 14:58:56 -0400

    test move

--- services/faa_data_service.js
+++ services/faa_data_service.js
@@ -4,24 +4,40 @@ var rp = require('request-promise');
 var ENDPOINT = 'http://services.faa.gov/airport/status/';
 
 function FAADataHelper() {};
-
-FAADataHelper.prototype.requestAirportStatus = function(airportCode){
-    return this.getAirportStatusCode(airportCode).then(function(response){
+FAADataHelper.prototype.requestAirportStatus = function (airportCode) {
+    return this.getAirportStatusCode(airportCode).then(function (response) {
         console.log('success - received airport info for ' + airportCode);
         return response.body;
     });
 };
-
-FAADataHelper.prototype.getAirportStatusCode = function(airportCode){
+FAADataHelper.prototype.getAirportStatusCode = function (airportCode) {
     var options = {
-        method: 'GET',
-        uri: ENDPOINT + airportCode,
-        resolveWithFullResponse: true,
-        json: true
+        method: 'GET'
+        , uri: ENDPOINT + airportCode
+        , resolveWithFullResponse: true
+        , json: true
     };
-    
     return rp(options);
 };
-
-
+FAADataHelper.prototype.formatAirportStatus = function (response) {
+    var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
+        weather: response.weather.weather
+        , temp: response.weather.temp
+        , wind: response.weather.wind
+    });
+    if (response.delay === 'true') {
+        return _.template('There is currently a delay for ${airport}. The average delay time is ${delay_time}. Delay is because of the following: ${delay_reason}. ${weather}')({
+            airport: response.name
+            , delay_time: response.status.avgDelay
+            , delay_reason: response.status.reason
+            , weather: weather
+        });
+    }
+    else {
+        return _.template('There is currently no delay at ${airport}. ${weather}')({
+            airport: response.name
+            , weather: weather
+        });
+    }
+};
 module.exports = FAADataHelper;
--- test/test_faa_data_helper.js
+++ test/test_faa_data_helper.js
@@ -5,7 +5,6 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 var FAADataHelper = require('../services/faa_data_service');
 chai.config.includeStack = true;
-
 describe('FAADataHelper', function () {
     var subject = new FAADataHelper()
     var airport_code;
@@ -30,8 +29,9 @@ describe('FAADataHelper', function () {
         });
     });
     describe('#formatAirportStatus', function () {
-        before(function () {
-            var status = {
+        var status;
+        beforeEach(function () {
+            status = {
                 'delay': 'true'
                 , 'name': 'Hartsfield-Jackson Atlanta International'
                 , 'ICAO': 'KATL'
@@ -60,11 +60,17 @@ describe('FAADataHelper', function () {
                 }
             };
         });
-        context('with a status containing no delay', function(){
-            it('formats the status as expected');
+        context('with a status containing no delay', function () {
+            it('formats the status as expected', function () {
+                status.delay = 'false';
+                expect(subject.formatAirportStatus(status)).to.eq('There is currently no delay at Hartsfield-Jackson Atlanta International. The current weather conditions are Light Snow, 36.0 F (2.2 C) and wind Northeast at 9.2mph.');
+            });
         });
-        context('with a status containing a delay', function(){
-            it('formats the status as expected');
-        })
+        context('with a status containing a delay', function () {
+            it('formats the status as expected', function () {
+                status.delay = 'true';
+                expect(subject.formatAirportStatus(status)).to.eq('There is currently a delay for Hartsfield-Jackson Atlanta International. The average delay time is 57 minutes. Delay is because of the following: AIRLINE REQUESTED DUE TO DE-ICING AT AIRPORT / DAL AND DAL SUBS ONLY. The current weather conditions are Light Snow, 36.0 F (2.2 C) and wind Northeast at 9.2mph.');
+            });
+        });
     });
 });
